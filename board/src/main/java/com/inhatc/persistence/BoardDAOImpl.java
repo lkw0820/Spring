152544d4bd39c5ddc4b07f9af23c6d37550cd902
lkw0820 <gewonlee99@naver.com>
2022-05-30 23:26:42 +0900

    search

--- board/src/main/java/com/inhatc/persistence/BoardDAOImpl.java
+++ board/src/main/java/com/inhatc/persistence/BoardDAOImpl.java
@@ -6,65 +6,78 @@ import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
-
 import com.inhatc.domain.BoardVO;
 import com.inhatc.domain.Criteria;
+import com.inhatc.domain.SearchCriteria;
+
 @Repository
-public class BoardDAOImpl implements BoardDAO{
+public class BoardDAOImpl implements BoardDAO {
 
-	@Inject
-	private SqlSession sqlSession;
-	private static final String namespace = "com.inhatc.mapper.BoardMapper";
-	@Override
-	public void create(BoardVO vo) throws Exception {
-		sqlSession.insert(namespace+".create",vo);
-		
-	}
+  @Inject
+  private SqlSession session;
 
-	@Override
-	public BoardVO read(int bno) throws Exception {
-		
-		return sqlSession.selectOne(namespace+".read",bno);
-	}
+  private static String namespace = "com.inhatc.mapper.BoardMapper";
 
-	@Override
-	public void update(BoardVO vo) throws Exception {
-		sqlSession.update(namespace+".update",vo);
-		
-	}
+  @Override
+  public void create(BoardVO vo) throws Exception {
+    session.insert(namespace + ".create", vo);
+  }
 
-	@Override
-	public void delete(int bno) throws Exception {
-		sqlSession.delete(namespace+".delete",bno);
-		
-	}
+  @Override
+  public BoardVO read(Integer bno) throws Exception {
+    return session.selectOne(namespace + ".read", bno);
+  }
 
-	@Override
-	public List<BoardVO> listAll() throws Exception {
-		
-		return sqlSession.selectList(namespace+".listAll");
-	}
+  @Override
+  public void update(BoardVO vo) throws Exception {
+    session.update(namespace + ".update", vo);
+  }
 
-	@Override
-	public List<BoardVO> listPage(int page) throws Exception {
-		if(page<=0) {
-			page = 1;
-		}
-		page = (page-1)*10;
-		return sqlSession.selectList(namespace+".listPage",page);
-	}
-	@Override
-	public List<BoardVO> listCriteria(Criteria cri) throws Exception {
-		if(cri.getPage()<=0) {
-			cri.setPage(1);
-		}
-		cri.setPage((cri.getPage()-1)*10);
-		return sqlSession.selectList(namespace+".listCriteria",cri);
-	}
+  @Override
+  public void delete(Integer bno) throws Exception {
+    session.delete(namespace + ".delete", bno);
+  }
 
-	@Override
-	public int countPaging(Criteria cri) throws Exception {
-		return sqlSession.selectOne(namespace+".countPaging", cri);
-	}
+  @Override
+  public List<BoardVO> listAll() throws Exception {
+    return session.selectList(namespace + ".listAll");
+  }
+
+  @Override
+  public List<BoardVO> listPage(int page) throws Exception {
+
+    if (page <= 0) {
+      page = 1;
+    }
+
+    page = (page - 1) * 10;
+
+    return session.selectList(namespace + ".listPage", page);
+  }
+
+  @Override
+  public List<BoardVO> listCriteria(Criteria cri) throws Exception {
+
+    return session.selectList(namespace + ".listCriteria", cri);
+  }
+
+  @Override
+  public int countPaging(Criteria cri) throws Exception {
+
+    return session.selectOne(namespace + ".countPaging", cri);
+  }
+
+  @Override    
+  public List<BoardVO> listSearch(SearchCriteria cri) throws Exception {
+
+    return session.selectList(namespace + ".listSearch", cri);
+  }
+
+  @Override
+  public int listSearchCount(SearchCriteria cri) throws Exception {
+
+    return session.selectOne(namespace + ".listSearchCount", cri);
+  }
+
 
 }
--- board/src/main/java/com/inhatc/service/BoardService.java
+++ board/src/main/java/com/inhatc/service/BoardService.java
@@ -4,16 +4,30 @@ import java.util.List;
 
 import com.inhatc.domain.BoardVO;
 import com.inhatc.domain.Criteria;
-
+import com.inhatc.domain.SearchCriteria;
 
 public interface BoardService {
-	
-	public void regist(BoardVO vo) throws Exception;
-	public BoardVO read(Integer bno) throws Exception;
-	public void modify(BoardVO vo) throws Exception;
-	public void remove(Integer bno) throws Exception;
-	public List<BoardVO> listAll() throws Exception;
-	public List<BoardVO> listCriteria(Criteria cri) throws Exception;
-	public int listCountCriteria(Criteria cri) throws Exception;
+
+  public void regist(BoardVO board) throws Exception;
+
+  public BoardVO read(Integer bno) throws Exception;
+
+  public void modify(BoardVO board) throws Exception;
+  
+  public void modifyPage(BoardVO board) throws Exception;
+
+  public void remove(Integer bno) throws Exception;
+
+  public List<BoardVO> listAll() throws Exception;
+
+
+  public List<BoardVO> listCriteria(Criteria cri) throws Exception;
+
+  public int listCountCriteria(Criteria cri) throws Exception;
+
+  public List<BoardVO> listSearchCriteria(SearchCriteria cri) 
+      throws Exception;
+
+  public int listSearchCount(SearchCriteria cri) throws Exception;
 
 }
